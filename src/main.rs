eb723c5fdce72f6cbf29b67f3bbef029bcb9054b
jefferson <jefferson.science@gmail.com>
2023-02-07 21:34:16 -0300

    Versão 1.0.0
    - Inserida opção -x que é capaz de separar
    nomes em CamelCase.
    - Chaveamento lógico colocado fora do .map()
    - Limpeza do código e recompilado otimizado.

--- src/main.rs
+++ src/main.rs
@@ -4,47 +4,37 @@ use diacritics::*;
 use std::*;
 use entrada::*;
 use regex::Regex;
-
-fn repetidos(input: &str, r: bool) -> String {
- let re = Regex::new(r"[\.-]{1,10}").unwrap_or_else(|e| {
-    panic!("A função 'repetidos' recebeu um argumento inválido: {}", e);
-    });
- if r == true{
-    return re.replace_all(input, "-").to_string()
-    }else{return input.to_string()}
-}
-
-fn estranhos(input: &str, e: bool) -> String {
-  let re = Regex::new(r"[^\w \.-]").unwrap_or_else(|e| {
-    panic!("A função 'estranhos' recebeu um argumento inválido: {}", e);
-  });
-if e == true{
-    return re.replace_all(input, "").to_string()
-    }else{return input.to_string()}
-}
-
-fn espacos(input: &str, s: bool) -> String {
-  let re = Regex::new(r"[ _]").unwrap_or_else(|e| {
-    panic!("A função 'espacos' recebeu um argumento inválido: {}", e);
-  });
-if s == true{
-    return re.replace_all(input, "-").to_string()
-    }else{return input.to_string()}
-}
-
-fn separacao(input: &str) -> String { // conserta repeticoes de .- ou -.
-    let re = Regex::new(r"[\.-][\.-]").unwrap_or_else(|e| {
-    panic!("A função 'separacao' recebeu um argumento inválido: {}", e);
-    });
+fn repetidos(input: &str) -> String {
+    let re = Regex::new(r"[\.-]{1,10}").unwrap();
     re.replace_all(input, "-").to_string()
 }
-fn pontos(input: &str, p: bool) -> String { 
-    let re = Regex::new(r"\.").unwrap_or_else(|e| {
-    panic!("A função 'pontos' recebeu um argumento inválido: {}", e);
-    });
-    if p == true{
-        return re.replace_all(input, "-").to_string()
-    }else{return input.to_string()}
+fn estranhos(input: &str) -> String {
+    let re = Regex::new(r"[^\w \.-]").unwrap();
+    re.replace_all(input, "").to_string()
+}
+fn espacos(input: &str) -> String {
+    let re = Regex::new(r"[ _]").unwrap();
+    re.replace_all(input, "-").to_string()
+}
+fn pontos(input: &str) -> String { 
+    let re = Regex::new(r"\.").unwrap();
+    re.replace_all(input, "-").to_string()
+}
+fn separacao(input: &str) -> String { // conserta repeticoes de .- ou -.
+    let re = Regex::new(r"[\.-][\.-]").unwrap();
+    re.replace_all(input, "-").to_string()
+}
+fn separador_maiusculas(input: &str) -> String {
+    let mut result = String::new();
+    let input_vec: Vec<char> = input.chars().collect();
+    result.push(input_vec[0]);
+    for character in &input_vec[1..] {
+        if character.is_uppercase() {
+            result.push('-');
+            result.push(*character);
+        }else{ result.push(*character)} 
+    }
+    result
 }
 fn finaliza(input: &str) -> String { // conserta finais de .- ou -.
     input.trim_end_matches(|c: char| c.is_alphanumeric() == false).to_string()
@@ -55,8 +45,8 @@ fn minusculas(input: &str) -> String {
 
 fn main() {
     let mut linhas: Vec<String> = entrada();
-    let (mut d, mut s, mut r, mut e, mut p, mut l, mut nada) = 
-        (false, false, false, false, false, false, false); 
+    let (mut d, mut s, mut r, mut e, mut p, mut l, mut x, mut nada) = 
+        (false, false, false, false, false, false, false, false); 
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {nada = true};
     if nada == false {
@@ -66,16 +56,18 @@ fn main() {
        if args[1].contains("p") {p = true};
        if args[1].contains("e") {e = true};
        if args[1].contains("l") {l = true};
+       if args[1].contains("x") {x = true};
     };
     linhas = linhas.into_iter()
-                    .map(|x| if d == true {remove_diacritics(&x)}else{x})
-                    .map(|x| if l == true {minusculas(&x)}else{x})
-                    .map(|x| estranhos(&x, e))
-                    .map(|x| espacos(&x, s))
-                    .map(|x| pontos(&x, p))
-                    .map(|x| separacao(&x))
-                    .map(|x| finaliza(&x))
-                    .map(|x| repetidos(&x, r))
+                    .map(|linha| if e == true {estranhos(&linha)}else{linha})
+                    .map(|linha| if s == true {espacos(&linha)}else{linha})
+                    .map(|linha| if p == true {pontos(&linha)}else{linha})
+                    .map(|linha| if x == true {separador_maiusculas(&linha)}else{linha})
+                    .map(|linha| if r == true {repetidos(&linha)}else{linha})
+                    .map(|linha| if d == true {remove_diacritics(&linha)}else{linha})
+                    .map(|linha| if l == true {minusculas(&linha)}else{linha})
+                    .map(|linha| separacao(&linha))
+                    .map(|linha| finaliza(&linha))
                     .collect();
     for linha in linhas {
         println!("{}", linha);
